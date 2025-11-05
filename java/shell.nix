2487b147271562fc6b8d800c251426fdaaf56d82
GouravSehgal7 <gouravsehgal.nsutcse@gmail.com>
2025-11-05 13:25:41 +0530

    new commit

--- java/shell.nix
+++ java/shell.nix
@@ -5,3 +5,7 @@ pkgs.mkShell {
     pkgs.openjdk17   # change to openjdk8, openjdk21, etc. if needed
   ];
 }
+
+# this message is for girl having birthday 3 march or 6 march may be i am not sure its been 5 year i dont remember exactly
+# can you recommend better option then nix OS i by mistakely currepted my java compiler
+# please make an issue in my github with possible solution
