591b9fc281b3f2774e429178d27ca58014a6cfb7
GouravSehgal7 <gouravsehgal.nsutcse@gmail.com>
2026-08-11 20:18:44 +0530

    lazy prop sgt

--- java/segmenttree/St.java
+++ java/segmenttree/St.java
@@ -37,8 +37,8 @@ public class St {
             }
             // partial overlap
             int mid = (l+r)/2;
-            int left = query(i, l, mid, ql, qr);
-            int right = query(i, mid+1, r, ql, qr);
+            int left = query(2*i+1, l, mid, ql, qr);
+            int right = query(2*i+2, mid+1, r, ql, qr);
             return left+right;
         }
 
@@ -105,6 +105,97 @@ public class St {
             tree[i] = Math.min(tree[2*i+1],tree[2*i+2]);
         }
     }
+    
+    public class SumSegmentwithLazyProp{
+
+        int tree[];
+        int arr[];
+        int lazy[];
+        SumSegmentwithLazyProp (int n,int arr[]){
+            this.tree = new int[4*n];
+            this.arr = arr;
+            this.lazy = new int[n*4];
+            this.build(0, 0, arr.length-1);
+        }
+
+        public void build(int i , int l, int r){
+            if(l == r){
+                this.tree[i] = this.arr[l];
+                return;
+            } 
+            int mid = (l+r)/2;
+            build(2*i+1, l, mid);
+            build(2*i+2, mid+1, r);
+            this.tree[i] = this.tree[2*i+1]+this.tree[2*i+2];
+        }
+
+        public int query(int i, int l , int r , int ql , int qr){
+            // non overlaping
+            if(ql>r || qr<l){
+                return 0;
+            }
+            // complete overlaping 
+            if(ql<=l && r <= qr){
+                return this.tree[i];
+            }
+            push(i, l, r);
+            // partial overlap
+            int mid = (l+r)/2;
+            int left = query(2*i+1, l, mid, ql, qr);
+            int right = query(2*i+2, mid+1, r, ql, qr);
+            return left+right;
+        }
+
+        public void updatepoint(int i , int l , int r , int pos , int val){
+            if(l == r){
+                this.tree[i] = val;
+                this.arr[r] = val;
+                return;
+            }
+            int mid = (l + r) / 2;
+            if(pos <= mid){
+                updatepoint(2*i+1, l, mid, pos, val);
+            }else{
+                updatepoint(2*i+2, mid+1, r, pos, val);
+            }
+            tree[i] = tree[2*i+1] + tree[2*i+2];
+        }
+
+        public void updateRange(int i , int l , int r , int ul , int ur , int val){
+            // no overlap
+            if(ul>r || ur <l) return;
+            // complete overlap
+            if(ul<=l && r <= ur){
+                tree[i] += (r-l+1)*val;
+                lazy[i]+=val;
+                return;
+            }
+            // partial overlap
+            push(i, l, r);
+            int mid = (l+r)/2;
+            updateRange(2*i+1, l, mid, ul, ur, val);
+            updateRange(2*i+2, mid+1, r, ul, ur, val);
+            tree[i] = tree[2 * i + 1] + tree[2 * i + 2];
+        }
+
+        public void push(int i, int l, int r) {
+
+            if (lazy[i] == 0) {
+                return;
+            }
+            tree[i] += (r - l + 1) * lazy[i];
+            if (l != r) {
+                lazy[2 * i + 1] += lazy[i];
+                lazy[2 * i + 2] += lazy[i];
+            }
+            lazy[i] = 0;
+        }
+
+    }
+
+
+    
+    
     public static void main(String[] args) {
         
     }
